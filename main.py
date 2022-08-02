bf1aef875be2ec0016e929d664fbd3f5cb329771
matejnesuta <matejnetusa@gmail.com>
2022-08-02 13:27:02 +0200

    help print

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ import datetime
 from bs4 import BeautifulSoup as bs
 
 def help():
-    print("help")
-    sys.exit
+    print("This script scrapes the IDOS web page for Brno and outputs the scheduled buses and trams on the stdout.")
+    print("\nusage:")
+    print("python main.py -b starting_destination -e end_destination [ options ] [ direct ]\n")
+    print("python main.py -h")
+    print("Required flags:")
+    print("\t-b [bus_stop]\t - sets the location we want to take a bus/from.")
+    print("\t-e [bus_stop]\t - sets our desired end location.")
+    print("\nOptional arguments:\n")
+    print("\t-d DD.MM.YYYY\t - sets the date of the journey (takes system date if flag is not set)")
+    print("\t-h | --help\t - prints help (this has priority over the rest of the flags)")
+    print("\t-t HH:MM\t - sets the time of bus/tram arrival to the starting location (takes system time if flag is not set)")
+    print("\t--direct\t - sets if the route should be direct or not (default is false)")
+    print("\n")
+    sys.exit(0)
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -21,12 +33,12 @@ def parse():
     direct = "false"
     
     try:
-        opts, args = getopt.getopt(argv,"hb:e:d:t:","direct")
+        opts, args = getopt.getopt(argv,"hb:e:d:t:",["direct","help"])
     except getopt.GetoptError:
         eprint("Unknown parameters!")
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
+        if opt == "-h" or opt == "--help":
             help()            
         elif opt in ("-b"):
             beginning = arg
