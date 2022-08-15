5d74942d60d32887cbfcae1d83d244b01ddfdfb1
matejnesuta <matejnetusa@gmail.com>
2022-08-16 01:32:29 +0200

    we scrapin

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def parse():
         eprint("Overlapping stations. Aborting the program.")
         sys.exit(3)
     else:
-        base = "https://idos.idnes.cz/brno/spojeni/vysledky/"
+        base = "https://idos.idnes.cz/en/brno/spojeni/vysledky/"
         entry = f"?f={beginning}&t={end}&direct={direct}"
     if (time):
         if (re.match("^(([0-1][0-9])|2[0-3]):[0-5][0-9]$", time)):
@@ -60,5 +60,34 @@ def parse():
 def scrape():
     r = parse()
     print(r.url)
-
+    soup = bs(r.text, 'html.parser')
+    divs = soup.find("div", { "class" : "connection-list" }).findAll("div", { "class" : "box"}, recursive=False)
+    for div in divs:
+        header = div.find("h2")
+        date = header.find("span").extract().string
+        arrival = header.string
+        dot = date.find(".")
+        if date[4]==".":
+            date = date[:dot+1] + "0" + date[dot+1:]
+            date = date[:5] + date[6:]
+        date = date.replace(".","-")
+        p = div.find("p").text
+        print("-"*80)
+        print("|", arrival, "|", date, "|",p)
+        print("-"*80)
+        vehicles = div.findAll("div", { "class" : "outside-of-popup"})
+        for vehicle in vehicles:
+            name = vehicle.find("h3").text
+            print(name)
+            stations = vehicle.find("ul").text
+            print(stations)
+    # divs = soup.findAll("div", class_="title-container")
+    # print(divs)
+    # vehicle{
+        
+    # }
+    # departure = soup.find("div", class_="reset stations first last").find("span")
+    # print(departure)
+    # for child in div:
+    #     print(child)
 scrape()
